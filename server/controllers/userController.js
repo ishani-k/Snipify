21f9ae6903116d21918540541e65d2f3343b4520
ishani-k <ishanikundu.ik@gmail.com>
2025-07-29 01:12:54 +0530

    temp

---
+++ server/controllers/userController.js
@@ -0,0 +1,9 @@
+
+
+
+//Api controller func to manage clerk user w db
+//http://localhost:4000/api/user/webhooks
+
+const clerkWebhooks = async (req, res) => {
+    
+}
--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import connectDB from './configs/mongodb.js'
 
 //App Config
 
 const PORT = process.env.PORT || 4000
 const app = express()
+await connectDB()
 
 //Initialize middlewares
 app.use(express.json())
