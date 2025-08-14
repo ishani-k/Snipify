a2f6533c293a147d9686f5c28c5a46131a4c6f89
ishani-k <ishanikundu.ik@gmail.com>
2025-08-15 04:05:26 +0530

    temp

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,7 +6,7 @@ const userRouter = express.Router()
 
 
 userRouter.post('/webhooks', clerkWebhooks)
-userRouter.get('/credits', authUser, userCredits)
+userRouter.post('/credits', authUser, userCredits)
 userRouter.post('/pay-razor', authUser, paymentRazorpay)
 userRouter.post('/verify-razor', verifyRazorpay)
 
