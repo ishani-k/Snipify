81267eabc4513fa4c43f9462d2a7f84141f8e9c0
ishani-k <ishanikundu.ik@gmail.com>
2025-08-15 23:26:56 +0530

    temp

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,4 +1,4 @@
- import jwt from 'jsonwebtoken'
+/* import jwt from 'jsonwebtoken'
 
 //middleware func to decode jwt token to get clerkId
 
@@ -27,4 +27,40 @@ const authUser = async (req, res, next) => {
 
 
 export default authUser 
+*/
 
+
+
+import { clerkClient } from '@clerk/clerk-sdk-node';
+import userModel from '../models/userModel.js';
+
+const authUser = async (req, res, next) => {
+  try {
+    const token = req.headers.token;
+    if (!token) {
+      return res.json({ success: false, message: "Not Authorized ! Login Again." });
+    }
+
+    const token_decode = jwt.decode(token);
+    const clerkId = token_decode?.sub;
+    req.body.clerkId = clerkId;
+
+    // Fallback: if no MongoDB record, create it now
+    let user = await userModel.findOne({ clerkId });
+    if (!user) {
+      const clerkUser = await clerkClient.users.getUser(clerkId);
+      user = await userModel.create({
+        clerkId: clerkUser.id,
+        email: clerkUser.primaryEmailAddress?.emailAddress,
+        firstName: clerkUser.firstName,
+        lastName: clerkUser.lastName,
+        photo: clerkUser.imageUrl,
+      });
+    }
+
+    next();
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
